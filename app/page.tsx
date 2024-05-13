0bf19bbb237e42d0d5db1e2008ca97287ed866f6
Miku Kawata <kawata_miku@lightcafe.co.jp>
2024-05-13 16:21:35 +0900

    install Switch, Label components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-      <h1 className='text-3xl font-bold mb-7'>オンラインチャット相談アプリ</h1>
-      <Link href={'/chat'} className='text-xl bg-red-400 py-2 px-4 rounded-md text-slate-50 cursor-pointer hover:bg-red-300'>
-        お医者さんに相談する
-      </Link>
+    <main className='p-10'>
+      <div className='flex items-center space-x-2'>
+        <Switch id='doctor-mode' />
+        <Label htmlFor='doctor-mode'>管理者モード</Label>
+      </div>
+      <div className='flex min-h-screen flex-col items-center justify-center p-24'>
+        <h1 className='text-3xl font-bold mb-7'>オンラインチャット相談アプリ</h1>
+        <Link href={'/chat'} className='text-xl bg-red-400 py-2 px-4 rounded-md text-slate-50 cursor-pointer hover:bg-red-300'>
+          お医者さんに相談する
+        </Link>
+      </div>
     </main>
   );
 }
