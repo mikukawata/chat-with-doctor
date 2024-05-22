58795e3f1583f98607095c4fc3ad77255b54e0de
Doiverson <doiverson.canada@gmail.com>
2024-05-22 00:41:35 -0700

    added auth and websocket

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,26 +1,79 @@
+'use client';
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 import ChatList from '../components/ChatList';
+import { supabaseBrowser } from '@/lib/supabese/browser';
+import { MessageType } from '@/lib/types';
+import SignIn from '../components/SignIn';
+import { User } from '@supabase/supabase-js';
 
-async function getMessageList() {
-  const response = await fetch('http://localhost:3000/api/message', {
-    cache: 'no-store',
-  });
-
-  const messageList = await response.json();
-  return messageList;
+interface Payload {
+    new: MessageType;
 }
 
-const ChatPage = async () => {
-  const messageList = await getMessageList();
-  return (
-    <div className='flex min-h-screen flex-col items-center px-24 py-6'>
-      <Link href={'/'} className='cursor-pointer mr-auto'>
-        ←ホームへ戻る
-      </Link>
-      <ChatList messageList={messageList} />
-    </div>
-  );
+const ChatPage = () => {
+    const [messageList, setMessageList] = React.useState<MessageType[]>([]);
+    const [user, setUser] = React.useState<User | null>(null);
+    const supabase = supabaseBrowser();
+    const router = useRouter();
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await supabase.auth.getUser();
+
+            if (!data.user) {
+                router.push('/');
+                return;
+            }
+
+            setUser(data.user);
+            getMessageList();
+        }
+
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        // if payload.new is empty, please check this issue: https://github.com/supabase/supabase-js/issues/848
+        const channel = supabase
+            .channel('room1')
+            .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'message' }, (payload: Payload) => {
+                setMessageList((prev) => [...prev, payload.new]);
+            })
+            .subscribe();
+
+        return () => {
+            channel.unsubscribe();
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    async function getMessageList() {
+        const response = await fetch('http://localhost:3000/api/message', {
+            cache: 'no-store',
+        });
+
+        const messageList = await response.json();
+        setMessageList(messageList);
+    }
+
+    if (!user) {
+        return <div>loading...</div>;
+    }
+
+    return (
+        <div className="flex min-h-screen flex-col items-center px-24 py-6">
+            <div className="flex justify-around w-full">
+                <Link href={'/'} className="cursor-pointer mr-auto">
+                    ←ホームへ戻る
+                </Link>
+                <SignIn user={user} />
+            </div>
+            <ChatList messageList={messageList} />
+        </div>
+    );
 };
 
 export default ChatPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import Link from 'next/link';
+import React from 'react';
+import SignIn from './components/SignIn';
+import { supabaseServer } from '@/lib/supabese/server';
 
-export default function Home() {
-  return (
-    <main className='p-10'>
-      <div className='flex min-h-screen flex-col items-center justify-center p-24'>
-        <h1 className='text-3xl font-bold mb-7'>オンラインチャット相談アプリ</h1>
-        <Link href={'/chat'} className='text-xl bg-red-400 py-2 px-4 rounded-md text-slate-50 cursor-pointer hover:bg-red-300'>
-          お医者さんに相談する
-        </Link>
-      </div>
-    </main>
-  );
+export default async function Home() {
+    const supabase = supabaseServer();
+    const { data } = await supabase.auth.getUser();
+    return (
+        <main className="p-10">
+            <div className="flex min-h-screen flex-col items-center justify-center p-24">
+                <h1 className="text-3xl font-bold mb-7">オンラインチャット相談アプリ</h1>
+                <SignIn user={data.user}>お医者さんに相談する</SignIn>
+            </div>
+        </main>
+    );
 }
